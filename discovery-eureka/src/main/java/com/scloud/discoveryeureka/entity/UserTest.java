891cd3afb30ef5bd484935b54157e1bb8d1eac96
wenbingyang <yyxn23@163.com>
2020-06-19 14:07:31 +0800

    2.MGB测试工具

---
+++ discovery-eureka/src/main/java/com/scloud/discoveryeureka/entity/UserTest.java
@@ -0,0 +1,20 @@
+package com.scloud.discoveryeureka.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    /** 编号 */
+    private String userId;
+    /** 姓名 */
+    private String userName;
+    private String password;
+
+}
--- discovery-eureka/src/test/java/com/scloud/discoveryeureka/DiscoveryEurekaApplicationTests.java
+++ discovery-eureka/src/test/java/com/scloud/discoveryeureka/DiscoveryEurekaApplicationTests.java
@@ -1,13 +1,50 @@
 package com.scloud.discoveryeureka;
 
+import com.scloud.discoveryeureka.dao.RoutInfoMapper;
+import com.scloud.discoveryeureka.dao.UserMapper;
+import com.scloud.discoveryeureka.entity.RoutInfo;
+import com.scloud.discoveryeureka.entity.User;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.RouteMatcher;
+
+import java.util.List;
+
+//两个*代表任意两个包 com.demo.aaa.mapper,也可以扫到com.demo.aaa.bbb.mapper
 @SpringBootTest
+@MapperScan("com.scloud.**.dao")
 class DiscoveryEurekaApplicationTests {
+
+    private static Logger logger = LoggerFactory.getLogger(DiscoveryEurekaApplicationTests.class);
+
+    @Resource
+    UserMapper userMapper;
+
+    @Resource
+    RoutInfoMapper routInfoMapper;
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    public void queryByPrimaryKey() {
+        User userInfo = userMapper.selectByPrimaryKey("00d2d1ec5fa711ea8f809c5c8e0ec292");
+        logger.info("查询结果为：{}", userInfo);
+    }
+
+    @Test
+    public void queryByExample(){
+        RoutInfo routInfo = new RoutInfo();
+        List<RoutInfo> routInfolist = routInfoMapper.selectAll();
+        logger.info("查询结果为：{}", routInfolist);
+    }
+
 
 }
